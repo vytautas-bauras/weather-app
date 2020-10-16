dba37e6b48e41de5740bc3463258b0982a0e3592
Vytautas Bauras <vytautas.bauras@bitai.lt>
2020-10-16 15:18:10 +0300

    test hash map provider

---
+++ symfony/tests/WeatherService/WeatherDataProvider/Resolver/HashMapWeatherDataProviderResolverTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\WeatherService\WeatherDataProvider\WeatherDataProviderInterface;
+
+use App\WeatherService\WeatherDataProvider\Resolver\HashMapWeatherDataProviderResolver;
+use App\WeatherService\WeatherDataProvider\Resolver\WeatherDataProviderNotFoundException;
+use App\WeatherService\WeatherDataProvider\WeatherDataProviderInterface;
+use PHPUnit\Framework\TestCase;
+
+class HashMapWeatherDataProviderResolverTest extends TestCase
+{
+    /**
+     * @var HashMapWeatherDataProviderResolver
+     */
+    private $resolver;
+
+    /**
+     * @var WeatherDataProviderInterface
+     */
+    private $providerStub;
+
+    public function setUp()
+    {
+        $this->providerStub = $this->createMock(WeatherDataProviderInterface::class);
+        $this->providerStub->method('getProviderName')->willReturn('provider');
+
+        $this->resolver = new HashMapWeatherDataProviderResolver([$this->providerStub]);
+    }
+
+    public function testResolvesProviderByName()
+    {
+        $this->assertEquals(
+            $this->providerStub,
+            $this->resolver->resolve('provider')
+        );
+    }
+
+    public function testThrowsIfProviderDoesNotExist()
+    {
+        $this->expectException(WeatherDataProviderNotFoundException::class);
+        $this->resolver->resolve('no_such_provider');
+    }
+}
